93dbb5f7ee9b36d2740a64567d83eaae3b0b891d
Kirti2805 <kir2805@gmail.com>
2021-03-31 12:13:55 -0500

    Adding EmployeeDetails

---
+++ JavaProject/src/com/arjun/EmployeeDetails.java
@@ -0,0 +1,29 @@
+package com.arjun;
+public class EmployeeDetails 
+{
+	int empId;
+	String empName;
+	float empSal;
+
+	public EmployeeDetails(){}
+	public EmployeeDetails(int empId, String empName, float empSal)
+	{
+		this.empId = empId;
+		this.empName = empName;
+		this.empSal = empSal;
+	}
+
+	public void calculateSalary()
+	{
+		System.out.println("Employee Salary :"+(empSal+2000));
+	}
+
+	public static void main(String[] args) 
+	{
+		EmployeeDetails ed = new EmployeeDetails();
+		ed.calculateSalary();
+
+		EmployeeDetails ed1 = new EmployeeDetails(1001, "Raju", 25000.50f);
+		ed1.calculateSalary();
+	}
+}
---
+++ JavaProject/src/com/arjun/MyFirstJavaApp.java
@@ -0,0 +1,10 @@
+package com.arjun;
+
+public class MyFirstJavaApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome To Java...");
+	}
+
+}
